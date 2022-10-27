0d8b483f8f3baac0c698462ef536a2a712be43b3
tommaso.lazzaretti <tommaso.lazzaretti@gmail.com>
2022-10-27 20:11:11 +0200

    optimize import

--- screen/CheckDataUser.tsx
+++ screen/CheckDataUser.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, TextInput, View} from 'react-native';
+import {Text, TextInput, View} from 'react-native';
 import GoBackButton from '../component/GoBackButton';
 import globalStyles from '../Style';
 
